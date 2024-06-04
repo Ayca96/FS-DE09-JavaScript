c91e80108ae6fad10dad0d21f1052eca04f2f01a
Ayca96 <aycakubra1@gmail.com>
2024-06-04 11:34:02 +0200

    dom

---
+++ javascript/class-notes/JS-16-DOM-Intro/2-Events.js
@@ -0,0 +1,47 @@
+//? METHOD-1 (SADECE JAVASCRİPT TE YAZILAN YOL) 1.yol
+//?--------------------------------------------------------
+
+
+//* example1 onmouseover, onmouseout
+
+const js = document.querySelector("#js")  // birkac yerde kullanilacagi zaman const atamasi yapiyoruz. 
+const badi = document.querySelector("#badi")
+
+js.onmouseover=()=>{
+
+  badi.style.background="pink"
+  
+}
+
+js.onmouseout=()=>{
+
+  badi.style.background="orange"
+  
+}
+
+//* example2 onclick ondoubleclick(ondblclick olarak güncel suan)
+
+
+const bir = document.querySelector(".bir")
+const iki = document.querySelector(".iki")
+
+bir.onclick=()=>{
+
+bir.src="./img/logo2.png"
+iki.src="./img/js_logo.png"
+
+}
+
+iki.onclick=()=>{
+
+  bir.src= "./img/js_logo.png"
+  iki.src= "./img/logo2.png"
+}
+
+iki.ondblclick=()=>{
+
+iki.src="./img/iki.webp"
+
+}
+
+// ayni etikete hem onclick hem ondoubleclick vermek mantikli olmayabiliyor.
